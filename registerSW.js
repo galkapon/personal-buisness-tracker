6ef36f2939663132ab7db1e45f52ed6934944cd2
galkapon <galkapon@users.noreply.github.com>
2026-09-06 11:58:42 +0000

    deploy: 2f80424b93875620a35847abaa5c2bdabe554e29

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personal-buisness-tracker/sw.js', { scope: '/personal-buisness-tracker/' })})}
