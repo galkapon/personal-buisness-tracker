51b95adb2aa50374694d52c1a8febfe5d4e0c1d2
galkapon <90338204+galkapon@users.noreply.github.com>
2026-09-06 14:22:26 +0300

    Fix vite.config path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': '/src',
     },
   },
   plugins: [
